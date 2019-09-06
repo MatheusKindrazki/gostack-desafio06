eeaac610e61828c442def0476070cc80d62e9b93
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-05 21:05:47 -0300

    buscando usuario no github

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
+    'import/prefer-default-export': 'off'
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'react/jsx-props-no-spreading': 'off'
